33d8c4ddadd5ef230cca8f9a6874a8872431892a
lmwdi <133152106+lmwdi@users.noreply.github.com>
2023-06-10 02:08:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import urllib.parse
 import requests
 from prettytable import PrettyTable
 
@@ -13,28 +12,20 @@ telegram_link = f"https://t.me/{phone_number}"
 table = PrettyTable()
 table.field_names = ["Application", "Link", "Registered"]
 
-# Check if the phone number is registered in WhatsApp
+# Check if the phone number is registered in WhatsApp by sending a GET request to the link
 whatsapp_response = requests.get(whatsapp_link)
-if whatsapp_response.status_code == 200:
-    if 'phone' in whatsapp_response.text:
-        is_whatsapp_account = True
-        table.add_row(["WhatsApp", whatsapp_link, "✓"])
-    else:
-        is_whatsapp_account = False
-        table.add_row(["WhatsApp", whatsapp_link, "✕"])
+if whatsapp_response.status_code == 200 and 'phone' in whatsapp_response.text:
+    is_whatsapp_account = True
+    table.add_row(["WhatsApp", whatsapp_link, "✓"])
 else:
     is_whatsapp_account = False
     table.add_row(["WhatsApp", whatsapp_link, "✕"])
 
-# Check if the phone number is registered in Telegram
+# Check if the phone number is registered in Telegram by sending a GET request to the link
 telegram_response = requests.get(telegram_link)
-if telegram_response.status_code == 200:
-    if 'tgme_user_info_header_info' in telegram_response.text:
-        is_telegram_account = True
-        table.add_row(["Telegram", telegram_link, "✓"])
-    else:
-        is_telegram_account = False
-        table.add_row(["Telegram", telegram_link, "✕"])
+if telegram_response.status_code == 200 and 'tgme_user_info_header_info' in telegram_response.text:
+    is_telegram_account = True
+    table.add_row(["Telegram", telegram_link, "✓"])
 else:
     is_telegram_account = False
     table.add_row(["Telegram", telegram_link, "✕"])
