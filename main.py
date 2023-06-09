eb02e4c7b6b0f0606d928072354ad29f7f67abbd
lmwdi <133152106+lmwdi@users.noreply.github.com>
2023-06-10 01:56:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import requests
+from prettytable import PrettyTable
 
 # Get the mobile phone number from the user input
 phone_number = input("Please enter your mobile phone number: ")
@@ -8,14 +9,23 @@ phone_number = input("Please enter your mobile phone number: ")
 whatsapp_link = f"https://wa.me/{phone_number}"
 telegram_link = f"https://t.me/{phone_number}"
 
+# Create a table to display the results
+table = PrettyTable()
+table.field_names = ["Application", "Link", "Registered"]
+
 # Check if the phone number is registered in WhatsApp by sending a GET request to the link
 whatsapp_response = requests.get(whatsapp_link)
 is_whatsapp_account = whatsapp_response.status_code == 200
+
+# Add the WhatsApp result to the table
+table.add_row(["WhatsApp", whatsapp_link, "✓" if is_whatsapp_account else "✕"])
 
 # Check if the phone number is registered in Telegram by sending a GET request to the link
 telegram_response = requests.get(telegram_link)
 is_telegram_account = telegram_response.status_code == 200
 
-# Print the results
-print(f"WhatsApp link: {whatsapp_link} - Account{' exists' if is_whatsapp_account else ' does not exist'}")
-print(f"Telegram link: {telegram_link} - Account{' exists' if is_telegram_account else ' does not exist'}")
+# Add the Telegram result to the table
+table.add_row(["Telegram", telegram_link, "✓" if is_telegram_account else "✕"])
+
+# Print the table
+print(table)
